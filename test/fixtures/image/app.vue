a6b42ab8752fc77a434b4063a2b8f645bfa39e3d
IO-Fire <96652894+IO-Fire@users.noreply.github.com>
2025-11-23 21:48:18 +0000

    test: image link header Preloaded images in <head> and attributes

---
+++ test/fixtures/image/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <NuxtImg
+      provider="imgix"
+      src="/blog/woman-hat.jpg"
+      width="300"
+      height="500"
+      fit="cover"
+      :preload="{ fetchPriority: 'high' }"
+      :modifiers="{ auto: 'format,compress', crop: 'faces' }"
+    />
+</template>
+
+<script setup>
+</script>
+
+<style>
+</style>
---
+++ test/image.test.ts
@@ -0,0 +1,37 @@
+import { fileURLToPath } from 'node:url'
+import { describe, it, expect } from 'vitest'
+import { setup, $fetch } from '@nuxt/test-utils/e2e'
+
+describe('ssr', async () => {
+  await setup({
+    rootDir: fileURLToPath(new URL('./fixtures/image', import.meta.url)),
+  })
+
+  it('renders the index page', async () => {
+    // Get response to a server-rendered page with `$fetch`.
+    const html = await $fetch('/')
+    expect(html).toMatch(/<img .+[^>]>/)
+  })
+
+  it('index page has link tag with imagesrcset attr.', async () => {
+    // Get response to a server-rendered page with `$fetch`.
+    const html = await $fetch('/')
+    /**
+     * Link to image with `<href>`, `imagesrcset`, `fetchpriority`
+     **/  
+    expect(html).toMatch(/<link rel="preload" as="image" href=".+[^">]" imagesrcset=".+[^">]" fetchpriority="high">/)
+  })
+
+  it('returns page with link header', async () => {
+    // Get response to a server-rendered page with `$fetch`.
+    await $fetch('/', {
+      onResponse: (response) => {
+        const link = response.response.headers.get('link')
+        /**
+         * Link to image with `<href>`, `imagesrcset`, `fetchpriority`
+         **/  
+        expect(link).toMatch(/^<.+[^>]>; rel="preload"; as="image"; imagesrcset=".+[^"]"; fetchpriority="high", <\/_nuxt\//)
+      },
+    })
+  })
+})
